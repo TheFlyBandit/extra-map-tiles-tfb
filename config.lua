2261c4665f065cb9acc48fe2082b6d9c9aa22685
TheFlyBandit <102429379+TheFlyBandit@users.noreply.github.com>
2026-09-11 13:51:05 -0400

    Update tile configurations in config.lua

--- config.lua
+++ config.lua
@@ -8,10 +8,10 @@ config.radar_masks = "radar_masks"
 
 config.tiles = {
     ['1'] = {
-        txd = "extra_tiles_blue",
-        txn = "tile_1",
+        txd = "roxwood",
+        txn = "roxwood",
         x_offset = 0.0,
-        y_offset = 0.0,
+        y_offset = -1.0,
         x_scale = 1.0,
         y_scale = 1.0,
         rotation = 0.0,
@@ -19,10 +19,10 @@ config.tiles = {
         centered = false
     },
     ['2'] = {
-        txd = "extra_tiles_blue",
-        txn = "tile_2",
-        x = 360.0,
-        y = 8400.0,
+        txd = "roxwood",
+        txn = "roxwood2",
+        x_offset = -1.0,
+        y_offset = -1.0,
         x_scale = 1.0,
         y_scale = 1.0,
         rotation = 0.0,
@@ -30,48 +30,26 @@ config.tiles = {
         centered = false
     },
     ['3'] = {
-        txd = "extra_tiles_green_1",
-        txn = "tile_3",
-        x_offset = 2.0,
-        y_offset = 2.0,
+        txd = "roxwood",
+        txn = "roxwood3",
+        x_offset = -1.0,
+        y_offset = 0.0,
+        x_scale = 1.0,
+        y_scale = 1.0,
+        rotation = 0.0,
+        alpha = 100,
+        centered = false
+    },
+
+        ['4'] = {
+        txd = "cayo",
+        txn = "minimap_sea_5_5",
+        x = 4859.6,
+        y = -5099.9,
         x_scale = 1.0,
         y_scale = 1.0,
         rotation = 0.0,
         alpha = 100,
         centered = true
-    },
-    ['4'] = {
-        txd = "extra_tiles_green_1",
-        txn = "tile_4",
-        x = 5387.3438,
-        y = -3060.9375,
-        x_scale = 1.0,
-        y_scale = 1.0,
-        rotation = 60.0,
-        alpha = 100,
-        centered = false
-    },
-    ['5'] = {
-        txd = "extra_tiles_green_2",
-        txn = "tile_5",
-        x = -7163.4375,
-        y = 2317.9688,
-        x_scale = 0.5,
-        y_scale = 0.5,
-        rotation = 0.0,
-        alpha = 43,
-        centered = false
-    },
-    ['6'] = {
-        txd = "extra_tiles_green_2",
-        txn = "tile_6",
-        x = -4140.0,
-        y = -5100.0,
-        x_scale = 1.5,
-        y_scale = 1.0,
-        rotation = 332.0,
-        alpha = 50,
-        centered = true,
-        visible = true
     },
 }
